1c93b78bdd286bb0e4ca5daddf55bf1bf761d6ab
Gautham Sajith <gsajith@umich.edu>
2020-10-24 21:16:38 -0400

    More fixes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './components/App';
-import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
-import {blue} from '@material-ui/core/colors';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./components/App";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
-    palette: {
-        primary: blue,
-    },
+  palette: {
+    primary: blue,
+  },
 });
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <App />
-    </ThemeProvider>,
-    document.getElementById('react-page')
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("react-page"),
 );

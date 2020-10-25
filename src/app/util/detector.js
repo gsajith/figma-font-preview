28716fc35d26b34a0ea3c2bce55a4362ba7461b9
Gautham Sajith <gsajith@umich.edu>
2020-10-24 21:19:52 -0400

    Prettier more things

--- src/app/util/detector.js
+++ src/app/util/detector.js
@@ -1,24 +1,24 @@
-var Detector = function() {
+const Detector = function() {
   // a font will be compared against all the three default fonts.
   // and if it doesn't match all 3 then that font is not available.
-  var baseFonts = ["monospace", "sans-serif", "serif"];
+  const baseFonts = ["monospace", "sans-serif", "serif"];
 
   //we use m or w because these two characters take up the maximum width.
   // And we use a LLi so that the same matching fonts can get separated
-  var testString = "mmmmmmmmmmlli";
+  const testString = "mmmmmmmmmmlli";
 
   //we test using 72px font size, we may use any size. I guess larger the better.
-  var testSize = "72px";
+  const testSize = "72px";
 
-  var h = document.getElementsByTagName("body")[0];
+  const h = document.getElementsByTagName("body")[0];
 
   // create a SPAN in the document to get the width of the text we use to test
-  var s = document.createElement("span");
+  const s = document.createElement("span");
   s.style.fontSize = testSize;
   s.innerHTML = testString;
-  var defaultWidth = {};
-  var defaultHeight = {};
-  for (var index in baseFonts) {
+  const defaultWidth = {};
+  const defaultHeight = {};
+  for (const index in baseFonts) {
     //get the default width for the three base fonts
     s.style.fontFamily = baseFonts[index];
     h.appendChild(s);
@@ -28,13 +28,13 @@ var Detector = function() {
   }
 
   function detect(font) {
-    var detected = false;
-    for (var index in baseFonts) {
+    let detected = false;
+    for (const index in baseFonts) {
       s.style.fontFamily = font + "," + baseFonts[index]; // name of the font along with the base font for fallback.
       h.appendChild(s);
-      var matched =
-        s.offsetWidth != defaultWidth[baseFonts[index]] ||
-        s.offsetHeight != defaultHeight[baseFonts[index]];
+      const matched =
+        s.offsetWidth !== defaultWidth[baseFonts[index]] ||
+        s.offsetHeight !== defaultHeight[baseFonts[index]];
       h.removeChild(s);
       detected = detected || matched;
     }
